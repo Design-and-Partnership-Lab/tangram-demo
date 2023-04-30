e81fe43990f312371212f2af4611ac9abceb231d
Zotman03 <zhenglin.ning@outlook.com>
2023-04-30 16:26:46 -0700

    Now can retrieve answers from openai api, however, there's lots of bugs and need env to store the apikey

--- src/components/SpeechTranscription.js
+++ src/components/SpeechTranscription.js
@@ -7,6 +7,8 @@ import useClipboard from "react-use-clipboard";
 export default function SpeechTranscription() {
   const [textToCopy, setTextToCopy] = useState();
   const [isCopied, setCopied] = useClipboard(textToCopy);
+  const [response, setResponse] = useState(null);
+
   const startListening = () =>
     SpeechRecognition.startListening({
       continuous: true,
@@ -15,17 +17,60 @@ export default function SpeechTranscription() {
 
   const { transcript } = useSpeechRecognition();
 
+  // const handleTranscript = async () => {
+  //   openai.api_key = api_key;
+  //   const result = await openai.Completion.create({
+  //     model: "text-davinci-003",
+  //     prompt: transcript,
+  //     max_tokens: 100,
+  //     temperature: 0,
+  //   });
+
+  //   setResponse(result.choices[0].text);
+  // };
+
+  // const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+
+  const APIBODY = {
+    model: "text-davinci-003",
+    // prompt: prompt + message, we can now add a prompt
+    prompt: transcript,
+    max_tokens: 60,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  };
+
+  async function handleTranscript() {
+    await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify(APIBODY),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setResponse(data.choices[0].text);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <div>
       <h2 className="text-red-800">Speech to text Converter</h2>
       <br />
-      <p>
-        a react hook that converts speech from the microphone to tex and blah
-      </p>
+      <p>Press start listening and speak to record</p>
       <div onClick={() => setTextToCopy(transcript)}>{transcript}</div>
       <div>
         <button onClick={setCopied} className="bg-green-300">
-          Was it copied? {isCopied ? "Yes" : "No"}
+          {isCopied ? "It is copied Yes" : "It is copied? No"}
         </button>
         <button onClick={startListening} className="bg-purple-300">
           Start listening
@@ -36,7 +81,12 @@ export default function SpeechTranscription() {
         >
           Stop listening
         </button>
+        <button onClick={handleTranscript} className="bg-red-800">
+          Send the transcript
+        </button>
+        <p>{response}</p>
       </div>
+      {console.log(transcript)}
     </div>
   );
 }
